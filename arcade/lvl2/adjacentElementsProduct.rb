26aaa88570062bfa1d278392590ce3663f276721
Heitor Silva <heitor-silva@hotmail.com>
2017-08-05 15:43:07 -0300

    Add descriptions to solved problems

--- arcade/lvl2/adjacentElementsProduct.rb
+++ arcade/lvl2/adjacentElementsProduct.rb
@@ -1,3 +1,13 @@
+# Given an array of integers, find the pair of adjacent elements
+# that has the largest product and return that product.
+
+# Example
+
+# For inputArray = [3, 6, -2, -5, 7, 3], the output should be
+# adjacentElementsProduct(inputArray) = 21.
+
+# 7 and 3 produce the largest product.
+
 def adjacentElementsProduct(inputArray)
   largest = nil
   inputArray.each_with_index do |num, index|
@@ -7,4 +17,3 @@ def adjacentElementsProduct(inputArray)
   end
   return largest
 end
-
--- tournaments/CodeMasters tourney/alphabeticShift.rb
+++ tournaments/CodeMasters tourney/alphabeticShift.rb
@@ -1,4 +1,5 @@
-# Given a string, replace each its character by the next one in the English alphabet (z would be replaced by a).
+# Given a string, replace each its character by the next one
+# in the English alphabet (z would be replaced by a).
 
 # Example
 
@@ -7,10 +8,9 @@
 
 def alphabeticShift(inputString)
   outputString = ''
-  inputString.each_char  do |letter|
+  inputString.each_char do |letter|
     letter = letter == 'z' ? 'a' : letter.next
     outputString << letter
   end
   return outputString
 end
-
