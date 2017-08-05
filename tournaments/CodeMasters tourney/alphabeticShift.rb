9bd93290e4031f10847074336b9eeea340cc3f28
Heitor Silva <heitor-silva@hotmail.com>
2017-08-05 13:17:37 -0300

    Add one tournament solution [Ruby]

---
+++ tournaments/CodeMasters tourney/alphabeticShift.rb
@@ -0,0 +1,16 @@
+# Given a string, replace each its character by the next one in the English alphabet (z would be replaced by a).
+
+# Example
+
+# For inputString = "crazy", the output should be
+# alphabeticShift(inputString) = "dsbaz".
+
+def alphabeticShift(inputString)
+  outputString = ''
+  inputString.each_char  do |letter|
+    letter = letter == 'z' ? 'a' : letter.next
+    outputString << letter
+  end
+  return outputString
+end
+
