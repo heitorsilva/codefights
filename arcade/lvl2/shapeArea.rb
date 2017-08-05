215ea239b6b2915f509f9a47a8a756b9e4a1049a
Heitor Silva <heitor-silva@hotmail.com>
2017-08-05 15:43:32 -0300

    Add one arcade level 2 solution [Ruby]

---
+++ arcade/lvl2/shapeArea.rb
@@ -0,0 +1,26 @@
+# Below we will define an n-interesting polygon.
+# Your task is to find the area of a polygon for a given n.
+
+# A 1-interesting polygon is just a square with a side of length 1.
+# An n-interesting polygon is obtained by taking the n - 1-interesting polygon
+# and appending 1-interesting polygons to its rim, side by side.
+
+# Example
+
+# For n = 2, the output should be
+# shapeArea(n) = 5;
+# For n = 3, the output should be
+# shapeArea(n) = 13.
+
+def shapeArea(n)
+  return n if n == 1
+
+  area = 4 * (n-1)
+
+  if n > 1
+    n -= 1
+    area += shapeArea(n)
+  end
+
+  return area
+end
